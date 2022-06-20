8168aebbbe0d31e1777df846eb016d5cf6898276
Лариса Зубкова <larissazubkova@gmail.com>
2022-06-20 20:40:23 +0300

    Исправление исправлений по отображению комментариев

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -11,7 +11,7 @@ getMockPhotos.forEach(({likes, comments, url}) => {
   const pictureElement = pictureTemplate.cloneNode(true);
 
   pictureElement.querySelector('.picture__likes').textContent = likes;
-  pictureElement.querySelector('.picture__comments').innerHTML = comments.length;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
   pictureElement.querySelector('.picture__img').src = url;
   pictureFragment.appendChild(pictureElement);
 });
