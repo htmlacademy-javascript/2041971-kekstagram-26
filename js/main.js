2e508c945382d5da3d7b2ad1c894ee8cb4ecad1c
Лариса Зубкова <larissazubkova@gmail.com>
2022-07-05 22:34:00 +0300

    Подключила pristin

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-import { mockPhotos } from "./data.js";
-import { generateThumbnails } from "./thumbnails.js";
-import { initialPopup } from "./popup.js";
-import {uploadImgForm} from "./img-form.js";
+import { mockPhotos } from './data.js';
+import { generateThumbnails } from './thumbnails.js';
+import { initialPopup } from './popup.js';
+import {uploadImgForm} from './img-form.js';
 import './validation.js';
 
 generateThumbnails(mockPhotos);
